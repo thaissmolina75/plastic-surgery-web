b7a3f019a48e80d47be0e0af361f99f7f1ad2499
darn0C <conradremon7@gmail.com>
2020-07-01 17:14:08 -0300

    Finish about page implemented Express for routing

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req,res) {
+    res.sendFile(__dirname + "/public/routing/landingPage.html")
+})
+
+app.get("/landingPage.html", function (req,res) {
+    res.sendFile(__dirname + "/public/routing/landingPage.html")
+})
+
+app.get("/aboutPage.html", function (req,res) {
+    res.sendFile(__dirname + "/public/routing/aboutPage.html")
+})
+
+app.listen(3000, function (req,res) {
+    console.log("Server is listening on port 3000")
+})
